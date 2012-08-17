637f80f841740ee2ff380900fd1582a85bfdc8ee
Raphaël Proust <raphlalou@gmail.com>
2012-08-17 13:20:25 +0200

    s/\t/  / s/\s\+$//
    This is purely cosmetics

--- src/zzz-sldm-profile.sh
+++ src/zzz-sldm-profile.sh
@@ -1,9 +1,9 @@
 if [[ -z "$DISPLAY" && -z "$SSH_TTY" && $(tty) = /dev/tty* ]]; then
-	# Drop root to console
-	if ! [[ $EUID -eq 0 ]]; then
-		sldm
-		if (( $? == 0 )); then
-			exit 0
-		fi
-	fi
+  # Drop root to console
+  if ! [[ $EUID -eq 0 ]]; then
+    sldm
+    if (( $? == 0 )); then
+      exit 0
+    fi
+  fi
 fi
